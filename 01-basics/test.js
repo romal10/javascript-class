341340be00f700db5242407e45998d857537f0f8
Mohaiyudin Romal <45965609+romal10@users.noreply.github.com>
2024-10-16 15:42:41 +0200

    bafi

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,11 @@
-console.log("mohaiyudin")
+document.addEventListener("DOMContentLoaded", function() {
+    const fills = document.querySelectorAll('.progress-container .fill');
+
+    fills.forEach(fill => {
+        const percentage = fill.style.width;
+        fill.style.width = '0'; // Reset width
+        setTimeout(() => {
+            fill.style.width = percentage; // Animate to target width
+        }, 100); // Delay to allow transition to be visible
+    });
+});
